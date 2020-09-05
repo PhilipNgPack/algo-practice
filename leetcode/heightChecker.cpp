326e5986eeaeeb23eaa28b929c059ff542fc58b4
Philip Ng Pack <philipngpack@gmail.com>
2020-09-05 13:56:52 -0400

    Completed heightChecker problem

--- leetcode/heightChecker.cpp
+++ leetcode/heightChecker.cpp
@@ -2,26 +2,27 @@
 // Created by Philip Ng Pack on 9/3/20.
 //
 
-#include <iostream>
 #include <vector>
-// not a well defined problem. Others had similar issues.
+// used std::sort instead of making my own
+// Runtime: 4 ms, faster than 89.26% of C++ online submissions for Height Checker.
+// Memory Usage: 8.4 MB, less than 26.90% of C++ online submissions for Height Checker.
 int heightChecker(std::vector<int> &heights) {
+    std::vector<int> newHeights(heights);
     int studentsMoved = 0;
-    int temp = 0;
 
-    for (int i = 0; i < heights.size() - 1; i++) {
-        if (heights[i] > heights[i + 1]) {
-            temp = heights[i];
-            heights[i] = heights[i + 1];
-            heights[i + 1] = temp;
+    std::sort(newHeights.begin(), newHeights.end());
+
+    // checks indexes between arrays
+    for (int i = 0; i < heights.size(); i++) {
+        if(heights[i] != newHeights[i]) {
             studentsMoved++;
         }
     }
-    std::cout << studentsMoved << std::endl;
+
+    return studentsMoved;
 }
 
 int main() {
-    std::vector<int> aVector = {5, 1, 2, 3, 4};
+    std::vector<int> aVector = {1, 1, 4, 2, 1, 3};
     heightChecker(aVector);
-
 }
